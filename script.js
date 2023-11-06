84d05d693c6fa6506c6b3e46dd41cfa2c678ee75
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-11-06 15:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const submit = document.getElementById("submit");
+const check = document.getElementById("checkbox");
+
+submit.addEventListener("click",()=>{
+	alert(check.value);
+});
